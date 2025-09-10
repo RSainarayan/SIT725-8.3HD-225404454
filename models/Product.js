87e2e1c4840b3f2fb63192c4c054f7b23adf8b62
Nelkin Eldho <65266806+nelkin01@users.noreply.github.com>
2025-09-10 12:22:33 +1000

    commit with some modifications to product page incuding weights column

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,8 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   sku: { type: String, required: true, unique: true },
   quantity: { type: Number, default: 0 },
-  location: { type: String }
+  location: { type: String },
+  weight: { type: Number, default: null } // Added weight field
 }, { timestamps: true });
 
 module.exports = mongoose.model('Product', productSchema);
